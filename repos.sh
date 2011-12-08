2de1feabbc8e05969fbae6c5b52cc233d319e8ad
Andy Stewart <lazycat.manatee@gmail.com>
2011-12-08 23:30:05 +0800

    first commit

---
+++ repos.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+case "$1" in
+    "record" )
+        git commit -a
+        ;;
+    "pull" )
+        git pull origin master
+        ;;
+    "push" )
+        git push git@github.com:manateelazycat/lazycat-gs-theme.git
+        ;;
+    "revert" )
+        git checkout -- .
+        ;;
+    * ) 
+        echo "Help"
+        echo "./repos.sh record         => record patch"
+        echo "./repos.sh pull           => pull patch"
+        echo "./repos.sh push           => push patch"
+        echo "./repos.sh revert         => revert patch"
+        ;;
+    esac
